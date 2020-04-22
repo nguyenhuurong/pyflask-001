a50b36ecf974f35859f0e9393349d993a0ff6856
nguyenhuurong <51846617+nguyenhuurong@users.noreply.github.com>
2020-04-22 13:26:15 +0700

    Update pyflask_db_001.py

--- pyflask_db_001.py
+++ pyflask_db_001.py
@@ -58,6 +58,48 @@ def giaiptb1():
 class Parameters(Resource):
     def get(self, firstParam):
         return "Day la tam so " + firstParam
+    
+@app.route('/giaiptb2', methods=['GET'])
+import math
+def giaiptb2():
+    query_parameters = request.args
+    a = query_parameters.get("a")
+    b = query_parameters.get("b")
+    c = query_parameters.get("a")
+
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    str = "khong co nghiem"
+    kq = { "tt" : str }
+    
+    if (a == 0):
+        if (b == 0):
+            kq = { "tt" : str };
+        else:
+            x =  -b/a
+            str = "co 1 nghiem"
+            kq = { "tt" : str , "x" : x}
+        return;
+    delta = b * b - 4 * a * c;
+    if (delta > 0):
+        x1 = (float)((-b + math.sqrt(delta)) / (2 * a));
+        x2 = (float)((-b - math.sqrt(delta)) / (2 * a));
+        str = "co 2 nghiem"
+        kq = { "tt" : str , "x1" : x1,"x2" : x2}
+    elif (delta == 0):
+        x1 = (-b / (2 * a));
+        str = "co nghiem kep"
+        kq = { "tt" : str , "x1 = x2 = " : x1}
+    else:
+        str = "khong co nghiem"
+        kq = { "tt" : str }
+    return jsonify(kq)
+
+class Parameters(Resource):
+    def get(self, firstParam):
+        return "Day la tam so " + firstParam
+
 
 api = Api(app)
 api.add_resource(Parameters, '/parameters/<firstParam>') # Route_1
