f21e32698ac819afec037fa2bcd6aacf03aa4c32
nguyenhuurong <51846617+nguyenhuurong@users.noreply.github.com>
2020-05-05 17:31:01 +0700

    Update pyflask_db_001.py

--- pyflask_db_001.py
+++ pyflask_db_001.py
@@ -14,6 +14,17 @@ from flask import jsonify
 app = Flask(__name__)
 #db_connect = create_engine('sqlite:///dsNhanVien.db')
 
+import pymongo 
+from pymongo import MongoClient 
+
+app = Flask(__name__)
+#db_connect = create_engine('sqlite:///dsNhanVien.db')
+
+MONGO_URI = 'mongodb+srv://rong:rong@cluster0-f5qr6.mongodb.net/test?retryWrites=true&w=majority'
+cluster = MongoClient(MONGO_URI)
+
+db = cluster["ShopOnline"]
+
 @app.route('/')
 def  index():
     return "<h1> Flask DB 001 - Connecting !!! </h1>"
@@ -21,9 +32,18 @@ def  index():
 @app.route('/login', methods=['GET', 'POST'])
 def  login():
     query_parameters = request.args
-    vusername = query_parameters.get("username")
-    vpassword = query_parameters.get("password")
-    return render_template("login.html")
+    vusername = query_parameters.get("Id")
+    vpassword = query_parameters.get("HoTen")
+
+    ### ch-eck Account / Tài khoản USER
+    collection = db["NhanVien"]
+    results = collection.find({"Id":1001, "HoTen": "NguyenVanA"}) 
+
+    if len(results) == 1:
+        logined_flag = True
+        return render_template("home.html")
+    else:
+        return render_template("login.html"))
 
 @app.route('/profile')
 def  profile():
@@ -38,54 +58,6 @@ def api_filter():
     query_parameters = request.args
     return jsonify(query_parameters)
 
-@app.route('/giaiptb1', methods=['GET'])
-def giaiptb1():
-    query_parameters = request.args
-    a = query_parameters.get("a")
-    b = query_parameters.get("b")
-
-    a = int(a)
-    b = int(b)
-
-    str = "khong co nghiem"
-    
-    kq = { "tt" : str }
-
-    if a == 0 and b == 0:
-        str = "VSN"
-        kq = { "tt" : str }
-    elif a != 0:
-        x =  -b/a
-        str = "co 1 nghiem"
-        kq = { "tt" : str , "x" : x}
-    else:
-        str = "KoCoN"
-        kq = { "tt" : str }
-    return jsonify(kq)
-
-
-@app.route('/giaib2', methods=['GET'])
-def giaib2():
-    query_parameters = request.args
-    a = query_parameters.get("a")
-    b = query_parameters.get("b")
-    c = query_parameters.get("c")
-
-    a = float(a)
-    b = float(b)
-    c = float(c)
-
-    str = "chưa biết có nghiệm hay không ! "
-    
-    kq = { "Trạng thái" : str , "Hệ số" : (a, b, c) }
-    ### 
-    
-    return jsonify(kq)
-
-
-class Parameters(Resource):
-    def get(self, firstParam):
-        return "Day la tam so " + firstParam
 
 api = Api(app)
 api.add_resource(Parameters, '/parameters/<firstParam>') # Route_1
